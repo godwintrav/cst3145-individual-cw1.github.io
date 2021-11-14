0f6c508553b1c17cf96067e66298d08ab2f10053
godwintrav <godwintrav@gmail.com>
2021-11-14 05:06:34 +0100

    added sort functionality

---
+++ js/app.js
@@ -0,0 +1,124 @@
+new Vue({
+    el: '#app',
+    data: {
+        lessons: lessons,
+        cartItems: [
+
+        ],
+        showProduct: true,
+        searchTxt: '',
+        order: 'asc',
+        attribute: 'subject',
+        sortOrders: {
+            Ascending: 'asc',
+            Descending: 'desc'
+        },
+        sortAttributes: {
+            Subject: 'subject',
+            Location: 'location',
+            Price: 'price',
+            Spaces: 'spaces'
+        },
+    },
+    methods: {
+        addLessonToCart: function(id){
+            const lessonIndex = this.lessons.findIndex(lesson => lesson.id === id);
+            if(lessonIndex != -1){
+                this.lessons[lessonIndex].numOfSpaces -= 1;
+                this.cartItems.push(id);
+            } 
+            
+        },
+        updateShowProduct: function(){
+            this.showProduct = !this.showProduct;
+        },
+        removeLessonFromCart: function(index, id){
+            const lessonIndex = this.lessons.findIndex(lesson => lesson.id === id);
+            this.lessons[lessonIndex].numOfSpaces += 1;
+            this.cartItems.splice(index, 1);
+            
+        },
+        searchLessons: function(){
+            this.lessons = lessons;
+            this.lessons = this.lessons.filter(lesson => lesson.subject.toLowerCase().includes(this.searchTxt) == true || lesson.location.toLowerCase().includes(this.searchTxt));
+        },
+        fetchLesson: function(id){
+            const lessonIndex = this.lessons.findIndex(lesson => lesson.id === id);
+            if(lessonIndex == -1){
+                return;
+            } 
+            return this.lessons[lessonIndex];
+        }
+    },
+    computed: {
+        sortedLessons() {
+            function compareSubjectAsc(a, b) {
+                if(a.subject > b.subject) return 1;
+                if(a.subject < b.subject) return -1;
+                return 0;
+            }
+            function compareSubjectDesc(a, b) {
+                if(a.subject > b.subject) return -1;
+                if(a.subject < b.subject) return 1;
+                return 0;
+            }
+
+            function compareLocationAsc(a, b) {
+                if(a.location > b.location) return 1;
+                if(a.location < b.location) return -1;
+                return 0;
+            }
+            function compareLocationDesc(a, b) {
+                if(a.location > b.location) return -1;
+                if(a.location < b.location) return 1;
+                return 0;
+            }
+
+            function comparePriceAsc(a, b) {
+                if(a.price > b.price) return 1;
+                if(a.price < b.price) return -1;
+                return 0;
+            }
+            function comparePriceDesc(a, b) {
+                if(a.price > b.price) return -1;
+                if(a.price < b.price) return 1;
+                return 0;
+            }
+
+            function compareSpacesAsc(a, b) {
+                if(a.numOfSpaces > b.numOfSpaces) return 1;
+                if(a.numOfSpaces < b.numOfSpaces) return -1;
+                return 0;
+            }
+            function compareSpacesDesc(a, b) {
+                if(a.numOfSpaces > b.numOfSpaces) return -1;
+                if(a.numOfSpaces < b.numOfSpaces) return 1;
+                return 0;
+            }
+
+            if(this.order == "asc"){
+                if(this.attribute == "subject"){
+                    return this.lessons.sort(compareSubjectAsc);
+                } else if(this.attribute == "location"){
+                    return this.lessons.sort(compareLocationAsc);
+                } else if(this.attribute == "price"){
+                    return this.lessons.sort(comparePriceAsc);
+                } else{
+                    return this.lessons.sort(compareSpacesAsc);
+                }
+                
+            }else{
+                if(this.attribute == "subject"){
+                    return this.lessons.sort(compareSubjectDesc);
+                } else if(this.attribute == "location"){
+                    return this.lessons.sort(compareLocationDesc);
+                } else if(this.attribute == "price"){
+                    return this.lessons.sort(comparePriceDesc);
+                } else{
+                    return this.lessons.sort(compareSpacesDesc);
+                }
+            }
+        }
+    }
+
+});
---
+++ js/lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        id: 1001,
+        subject: 'Math',
+        location: 'London',
+        price: 170,
+        numOfSpaces: 5,
+        image: 'image/calc.png',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1002,
+        subject: 'English',
+        location: 'Cardiff',
+        price: 160,
+        numOfSpaces: 5,
+        image: 'image/english.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1003,
+        subject: 'Science',
+        location: 'Hendon',
+        price: 10,
+        numOfSpaces: 5,
+        image: 'image/science.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1004,
+        subject: 'Business',
+        location: 'America',
+        price: 50,
+        numOfSpaces: 5,
+        image: 'image/business.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1005,
+        subject: 'ICT',
+        location: 'Ibadan',
+        price: 900,
+        numOfSpaces: 5,
+        image: 'image/computer.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1006,
+        subject: 'Physics',
+        location: 'Lagos',
+        price: 150,
+        numOfSpaces: 5,
+        image: 'image/physics.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1007,
+        subject: 'Music',
+        location: 'Kano',
+        price: 120,
+        numOfSpaces: 5,
+        image: 'image/music.jpg',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1008,
+        subject: 'French',
+        location: 'Katsina',
+        price: 600,
+        numOfSpaces: 5,
+        image: 'image/pentool.png',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1009,
+        subject: 'Christian Religious Studies',
+        location: 'London',
+        price: 400,
+        numOfSpaces: 5,
+        image: 'image/crs.png',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1010,
+        subject: 'Chemistry',
+        location: 'London',
+        price: 200,
+        numOfSpaces: 5,
+        image: 'image/chemistry.jpg',
+        icon: 'fa fa-calculator'
+    },
+]
